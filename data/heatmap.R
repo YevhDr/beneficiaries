acec829fb09e2f20de17f302b9c4be55654e4239
YevhDr <evgesha.drozdova@gmail.com>
2017-09-21 12:54:04 +0300

    Initial commit

---
+++ data/heatmap.R
@@ -0,0 +1,32 @@
+getwd()
+setwd("D:/pages/beneficiaries/data")
+library(readr)
+library(stringr)
+library(dplyr)
+library(ggplot2)
+library(wesanderson)
+file <- "heatmap_data.csv"
+file2 <- "heatmap_data (1).csv"
+
+doc <- read.table(file, sep = ",", header = TRUE, encoding = "UTF-8")
+
+
+doc$colors <- cut(doc$presentation,breaks = c(min(doc$presentation), -10, -5, -3, 0, 3, 5, 10, 15,40),right = FALSE)
+
+p<-ggplot(doc, aes(x = party, y = bussinessman )) + 
+  geom_tile(aes(fill = colors), colour = "white") +
+  scale_fill_brewer(palette = "Reds") +
+  
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position="bottom",
+        legend.key.height = unit(0.4, "cm"),
+        legend.key.width = unit(1, "cm"),
+        axis.text.x = 
+          element_text(angle=45, 
+          hjust = 1,vjust=1,size = 7))
+  
+ggsave("heatmap.svg", plot = p,  
+       path = "D:/pages/beneficiaries/img", 
+       dpi = 300, limitsize = FALSE)
+  
