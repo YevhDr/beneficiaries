481761d5e4ef5b8781e8873e18411bbed15f9d6d
YevhDr <evgesha.drozdova@gmail.com>
2017-10-03 19:40:00 +0300

    +bar chart

--- js/barchart.js
+++ js/barchart.js
@@ -1,54 +1,3 @@
-var margin1 = {top: 10, right: 50, bottom: 20, left: 100};
-
-var widther = window.outerWidth;
-
-var width1 = widther - margin1.left - margin1.right,
-    height1 = 250 - margin1.top - margin1.bottom;
-
-//Appends the svg to the chart-container div
-var svg = d3.select(".g-chart").append("svg")
-    .attr("width", width1 + margin1.left + margin1.right)
-    .attr("height", height1 + margin1.top + margin1.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin1.left + "," + margin1.top + ")");
-
-//Creates the xScale
-var xScale = d3.scale.linear()
-    .range([0,width1]);
-
-//Creates the yScale
-var y0 = d3.scale.ordinal()
-    .rangeBands([height1, 0], 0.2)
-    .domain(["М.Фрідман", "Р.Ахметов", "А.Веревський", "О.Бахматюк", "С.Тігіпко", "NCH Capital", "В.Іванчик", "Ю.Косюк", "П.Порошенко", "К.Іаковідес"]);
-
-//Defines the y axis styles
-var yAxis = d3.svg.axis()
-    .scale(y0)
-    .orient("left");
-
-//Defines the y axis styles
-var xAxis = d3.svg.axis()
-    .scale(xScale)
-    .orient("bottom")
-    .tickFormat(function(d) {return d + "%"; })
-    .tickSize(height1)
-    .ticks(numTicks(width1));
-
-//Loads the data
-var file = "data/top10_businessman.csv";
-d3.csv(file, ready);
-
-function ready(err, data) {
-
-    if (err) throw "error loading data";
-    console.log("hello");
-
-    //FORMAT data
-    data.forEach(function(d) {
-        d.value = +d.value;
-    });
-
-    console.log(data);
 
     //Appends chart headline
     d3.select(".g-hed").text("ЛИШЕ БІЗНЕС ЧИ Й ПОЛІТИКА");
@@ -56,56 +5,6 @@ function ready(err, data) {
     //Appends chart intro text
     d3.select(".g-intro").text("Бізнесмени, з чиїми компаніями повʼязано найбільше місцевих депутатів");
 
-    //Sets the max for the xScale
-    var maxX = d3.max(data, function(d) { return d.value; });
-
-    //Defines the xScale max
-    xScale.domain([0, maxX ]);
-
-    //Appends the y axis
-    var yAxisGroup = svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis);
-
-    //Appends the x axis
-    var xAxisGroup = svg.append("g")
-        .attr("class", "x axis");
-        // .call(xAxis);
-
-    //Binds the data to the bars
-    var categoryGroup = svg.selectAll(".g-category-group")
-        .data(data)
-        .enter()
-        .append("g")
-        .attr("class", "g-category-group")
-        .attr("transform", function(d) {
-            return "translate(0," + y0(d.name) + ")";
-        })
-        ;
-
-    //Appends first bar
-    var bars = categoryGroup.append("rect")
-        .attr("width", function(d) { return xScale(d.value) / 2; })
-        .attr("height", y0.rangeBand() )
-        .attr("class", "g-num")
-        .attr("transform", "translate(0,4)");
-
-    //Binds data to labels
-    var labelGroup = svg.selectAll("g-num")
-        .data(data)
-        .enter()
-        .append("g")
-        .attr("class", "g-label-group")
-        .attr("transform", function(d) {
-            return "translate(0," + y0(d.name) + ")";
-        });
-
-    //Appends labels
-    var barLabels = labelGroup.append("text")
-        .text(function(d) {return  d.value;})
-        .attr("x", function(d) { return xScale(1)})
-        .attr("y", y0.rangeBand() )
-        .attr("class", "g-labels");
 
     //Appends chart source
     d3.select(".g-source-bold")
@@ -117,56 +16,191 @@ function ready(err, data) {
         .attr("class", "g-source-reg");
 
 
-    //RESPONSIVENESS
-    d3.select(window).on("resize", resized);
 
-    function resized() {
 
-        //new margin
-        var newMargin = {top: 10, right: 80, bottom: 20, left: 50};
 
-        //Get the width of the window
-        var w = d3.select(".g-chart").node().clientWidth;
-        console.log("resized", w);
 
-        //Change the width of the svg
-        d3.select("svg")
-            .attr("width", w);
+    var d3Container = d3.select("#d3-bar-horizontal"),
+        margin_bar = {top: 20, right: 10, bottom: 5, left: 80},
+        width_bar = d3Container.node().getBoundingClientRect().width - margin_bar.left - margin_bar.right,
+        height_bar = 300 - margin_bar.top - margin_bar.bottom - 5,
+        n = 12;
 
-        //Change the xScale
-        xScale
-            .range([0, w - newMargin.right]);
+    // Format data
+    var format = d3.format(",.0f");
 
-        //Update the bars
-        bars
-            .attr("width", function(d) { return xScale(d.value); });
 
-        //Updates bar labels
-        barLabels
-            .attr("x", function(d) { return xScale(1); })
-            .attr("y", y0.rangeBand() );
 
-        // //Updates xAxis
-        // xAxisGroup
-        //     .call(xAxis);
+    // Construct scales
+    // ------------------------------
 
-        //Updates ticks
-        xAxis
-            .scale(xScale)
-            .ticks(numTicks(w));
+    // Horizontal
+    var x = d3.scale.linear()
+        .range([0, width_bar]);
 
+    // Verticals
+    var y = d3.scale.ordinal()
+        .rangeRoundBands([0, height_bar], .1);
+
+    // Colors
+    var colors = d3.scale.category20();
+
+
+
+    // Create axes
+    // ------------------------------
+
+    // Horizontal
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("top")
+        .tickSize(-height_bar);
+
+    // Vertical
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickSize(5);
+
+
+
+    // Create chart
+    // ------------------------------
+
+    // Add SVG element
+    var container = d3Container.append("svg");
+
+    // Add SVG group
+    var svg = container
+        .attr("width", width_bar + margin_bar.left + margin_bar.right)
+        .attr("height", height_bar + margin_bar.top + margin_bar.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin_bar.left + "," + margin_bar.top + ")");
+
+
+
+    // Load data
+    // ------------------------------
+
+    d3.csv("data/top10_businessman.csv", function(data) {
+
+        // Parse numbers, and sort by value.
+        data.forEach(function(d) { d.value = +d.value; });
+        data.sort(function(a, b) { return b.value - a.value; });
+
+
+        // Set input domains
+        // ------------------------------
+
+        // Horizontal
+        x.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+        // Verticals
+        y.domain(data.map(function(d) { return d.name; }));
+
+
+        //
+        // Append chart elements
+        //
+
+        // Append axes
+        // ------------------------------
+
+        // Horizontal
+        svg.append("g")
+            .attr("class", "x axis")
+            // .call(xAxis)
+        ;
+
+        // Vertical
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+        // Remove lines
+        svg.selectAll(".d3-axis line, .d3-axis path").attr("stroke-width", 0);
+
+
+        // Append bars
+        // ------------------------------
+
+        // Group bars
+        var bar = svg.selectAll(".d3-bar-group")
+            .data(data)
+            .enter()
+            .append("g")
+            .attr("class", "d3-bar-group")
+            .attr("fill", "#fee08b")
+            .attr("transform", function(d) { return "translate(0," + y(d.name) + ")"; });
+
+        // Add bar
+        bar.append("rect")
+            .attr("class", "d3-bar")
+            .attr("width", function(d) { return x(d.value); })
+            .attr("height", y.rangeBand());
+
+        // Add text label
+        bar.append("text")
+            .attr("x", function(d) { return x(d.value); })
+            .attr("y", y.rangeBand() / 2)
+            .attr("dx", -10)
+            .attr("dy", ".35em")
+            .style("text-anchor", "end")
+            .style("fill", "#fff")
+            .style("font-size", 12)
+            .text(function(d) { return format(d.value); });
+    });
+
+
+
+    // Resize chart
+    // ------------------------------
+
+    // Call function on window resize
+    $(window).on('resize', resize);
+
+    // Call function on sidebar width change
+    $('.sidebar-control').on('click', resize);
+
+    // Resize function
+    //
+    // Since D3 doesn't support SVG resize by default,
+    // we need to manually specify parts of the graph that need to
+    // be updated on window resize
+    function resize() {
+
+        // Layout variables
+        width = d3Container.node().getBoundingClientRect().width - margin.left - margin.right;
+
+
+        // Layout
+        // -------------------------
+
+        // Main svg width
+        container.attr("width", width + margin.left + margin.right);
+
+        // Width of appended group
+        svg.attr("width", width + margin.left + margin.right);
+
+
+        // Axes
+        // -------------------------
+
+        // Horizontal range
+        x.range([0, width]);
+
+        // Horizontal axis
+        svg.selectAll('.d3-axis-horizontal')
+            // .call(xAxis)
+        ;
+
+
+        // Chart elements
+        // -------------------------
+
+        // Line path
+        svg.selectAll('.d3-bar').attr("width", function(d) { return x(d.value); });
+
+        // Text label
+        svg.selectAll('.d3-label-value').attr("x", function(d) { return x(d.value); });
     }
-
-}
-
-//Determines number of ticks base on width
-function numTicks(widther) {
-    if (widther <= 400) {
-        return 4
-        console.log("return 4")
-    }
-    else {
-        return 10
-        console.log("return 5")
-    }
-}
